a863690f58b7f9976738c03c01b8b0b87abbee7e
Sebastian Forsman <sebastian.forsman@avoine.fi>
2015-01-23 10:12:27 +0000

    Load default deployer from dhelper

--- contrib/profile.d/deploy.sh
+++ contrib/profile.d/deploy.sh
@@ -1,8 +1,11 @@
 # Deploys app root and creates the DB if necessary
-/app/.heroku/php/bin/php /app/deploy.php
+/app/.heroku/php/bin/php /app/vendor/sforsman/dhelper/bin/deploy.php
 
-# This is not needed after deployment is complete
-rm -f /app/deploy.php
+# Check for custom deployment script and remove it
+if [ -f /app/deploy.php ]; then
+  /app/.heroku/php/bin/php /app/deploy.php
+  rm -f /app/deploy.php
+fi
 
 # These are removed for security reasons
 unset PW_DB_ADMIN_USER
